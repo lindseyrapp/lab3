54fea922a80f38adaab5d1f641c8b4eadecb681e
lindseyrapp <lrappaport3139@student.palomar.edu>
2023-10-25 14:50:46 -0700

    changes

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,20 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+
+	@Test 
+	public void testReverseInPlace2() {
+    int[] input1 = { 1, 2};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{2, 1}, input1);
+	}
+
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = { 5,7};
+    assertArrayEquals(new int[]{7, 5}, ArrayExamples.reversed(input1));
+  }
+
 }
